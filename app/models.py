a5ef599ea78bfc2894883297a1b871e05b0e1ada
dev hadvani <devhadvani147@gmail.com>
2024-06-17 16:09:05 +0530

    created: story model

--- app/models.py
+++ app/models.py
@@ -104,9 +104,23 @@ class CommentLike(models.Model):
     def __str__(self):
         return f'{self.user.username} likes comment {self.comment.id}'
 
-# class Story(models.Model):
-#     media = models.ImageField(upload_to='story/')
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     expire_at = models.DateTimeField()
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='story')
+class Story(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stories')
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
 
+    def save(self, *args, **kwargs):
+        if not self.expires_at:
+            self.expires_at = timezone.now() + timedelta(hours=24)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Story by {self.user.email}'
+
+class StoryImage(models.Model):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='story_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Image for story {self.story.id}'
